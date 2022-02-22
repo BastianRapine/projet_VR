f88b985ca4ce459741cadd98b178f3cd2a856e93
clement-ghn <ent.clementgehin@gmail.com>
2022-02-22 09:46:21 -0500

    Chronomètre du jeu
    réalisation du chronomètre comptant le temps total de jeu

---
+++ Assets/Tempstotal.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tempstotal : MonoBehaviour
+{
+    public float timeValue = 1;
+    public Text timerText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue += Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 1;
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
